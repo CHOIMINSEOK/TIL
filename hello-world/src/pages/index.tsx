c81260b8afc9ae002e2a876e6abd9f8e2f469699
CHOIMINSEOK <cms3718@gmail.com>
2020-01-05 14:33:05 +0900

    styled-components 추가 및 간단한 타이틀 구현

--- hello-world/src/pages/index.tsx
+++ hello-world/src/pages/index.tsx
@@ -1,3 +1,28 @@
 import React from "react"
+import styled from "styled-components"
 
-export default () => <div>MINSEOK DATABASE</div>
+export default () => (
+  <Wrapper>
+    <Title>MSDB</Title>
+    <Divider />
+  </Wrapper>
+)
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-contents: center;
+`
+
+const Title = styled.div`
+  font-weight: 10;
+  font-size: 40px;
+`
+
+const Divider = styled.div`
+  width: 400px;
+  height: 2px;
+  margin-top: 16px;
+  background-color: #f7cac9;
+`
